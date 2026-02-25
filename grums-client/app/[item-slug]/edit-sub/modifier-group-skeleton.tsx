ea276c6f4aa35afe9b3875e4569ac356c0167932
YanivLev <hiy4niv@gmail.com>
2026-02-25 09:49:34 -0500

    Changed the modifier loading skeleton to show 3 chips on mobile view

--- grums-client/app/[item-slug]/edit-sub/modifier-group-skeleton.tsx
+++ grums-client/app/[item-slug]/edit-sub/modifier-group-skeleton.tsx
@@ -6,9 +6,9 @@ export default function ModifierGroupSkeleton() {
         <div className="mb-8">
             <Skeleton width={120} height={24} className="mb-4" />
             
-            <div className="flex gap-4">
+            <div className="flex gap-2 lg:gap-4">
                 {Array.from({ length: 4}).map((_, i) => (
-                    <div key={i} className="flex flex-col items-center gap-2 w-[80px]">
+                       <div key={i} className={`flex flex-col items-center gap-2 w-[80px] ${i === 3 ? 'hidden lg:flex' : ''}`}>
                         <Skeleton circle width={80} height={80} />
                         <Skeleton width={60} height={14} />
                     </div>
