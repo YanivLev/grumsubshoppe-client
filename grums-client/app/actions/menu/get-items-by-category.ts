a63826630cff1055ac6c635c0985402b88474fe4
YanivLev <hiy4niv@gmail.com>
2026-05-15 02:29:42 -0400

    Refactor get items by category id url

--- grums-client/app/actions/menu/get-items-by-category.ts
+++ grums-client/app/actions/menu/get-items-by-category.ts
@@ -1,13 +1,13 @@
 "use server"
 
-import {getById} from "@/app/common/util/server-requests";
+import { get } from "@/app/common/util/server-requests";
 
-export default async function getItemsByCategory(itemId: string) {
+export default async function getItemsByCategory(categoryId: string) {
     try {
-    const data = await getById(`clover/inventory/${itemId}`, itemId);
-    return data;
+        const data = await get(`clover/inventory/categories/${categoryId}`);
+        return data;
     } catch (error) {
-    console.error("Fetch Error:", error);
-    return [];
+        console.error("Fetch Error:", error);
+        return [];
     }
 }
