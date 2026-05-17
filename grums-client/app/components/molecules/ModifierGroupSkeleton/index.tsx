68e98895b09a37ac023ac87e1f477a93f1cdf424
YanivLev <hiy4niv@gmail.com>
2026-05-16 23:23:11 -0400

    Center the modifier loading skeleton

--- grums-client/app/components/molecules/ModifierGroupSkeleton/index.tsx
+++ grums-client/app/components/molecules/ModifierGroupSkeleton/index.tsx
@@ -4,9 +4,11 @@ import "react-loading-skeleton/dist/skeleton.css"
 export default function ModifierGroupSkeleton() {
     return (
         <div className="mb-8">
-            <Skeleton width={120} height={24} className="mb-4" />
+            <div className="flex justify-center lg:justify-start mb-4">
+                <Skeleton width={120} height={24} />
+            </div>
 
-            <div className="flex gap-2 lg:gap-4">
+            <div className="flex justify-center lg:justify-start gap-2 lg:gap-4">
                 {Array.from({ length: 4 }).map((_, i) => (
                        <div key={i} className={`flex flex-col items-center gap-2 w-[80px] ${i === 3 ? 'hidden lg:flex' : ''}`}>
                         <Skeleton circle width={80} height={80} />
