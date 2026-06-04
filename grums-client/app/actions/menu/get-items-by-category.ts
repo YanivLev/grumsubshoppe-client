54dfc9bfc752cde3cac1e551b0a6cc418e4f3073
YanivLev <hiy4niv@gmail.com>
2026-06-04 12:01:53 -0400

    Fix: added a promise of type item list, added the itemGroup as an optional field in Item interface

--- grums-client/app/actions/menu/get-items-by-category.ts
+++ grums-client/app/actions/menu/get-items-by-category.ts
@@ -1,8 +1,9 @@
 "use server"
 
 import { get } from "@/app/common/util/server-requests";
+import { IItem } from "@/app/common/interfaces/item.interface";
 
-export default async function getItemsByCategory(categoryId: string) {
+export default async function getItemsByCategory(categoryId: string) : Promise<IItem[]> {
     try {
         const data = await get(`clover/inventory/categories/${categoryId}`);
         return data;
--- grums-client/app/common/interfaces/item.interface.ts
+++ grums-client/app/common/interfaces/item.interface.ts
@@ -2,6 +2,9 @@ export interface IItem {
     id: string;
     name: string;
     price: number;
+    itemGroup?: {
+        id: string;
+    };
     categories?: {
         elements: { name: string }[];
     };
