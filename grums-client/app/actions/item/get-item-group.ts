e5b6d546149258c5c748002e640a7f41d0b253ec
YanivLev <hiy4niv@gmail.com>
2026-06-04 12:00:19 -0400

    Fix: added a promise of type ItemGroup

--- grums-client/app/actions/item/get-item-group.ts
+++ grums-client/app/actions/item/get-item-group.ts
@@ -1,13 +1,14 @@
 "use server"
 
+import { IItemGroup } from "@/app/common/interfaces/item-group.interface";
 import {getById} from "@/app/common/util/server-requests";
 
-export default async function getItemGroupById(itemGroupId: string) {
+export default async function getItemGroupById(itemGroupId: string) : Promise<IItemGroup> {
     try {
     const data = await getById(`clover/inventory/item-groups/${itemGroupId}`, itemGroupId);
     return data;
     } catch (error) {
     console.error("Fetch Error:", error);
-    return [];
+    throw error;
     }
 }
