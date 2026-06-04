379ea6b19b777fcbe9ae216bd1ac1eb864b2f126
YanivLev <hiy4niv@gmail.com>
2026-06-04 12:00:41 -0400

    Fix: added a promise of type Item

--- grums-client/app/actions/item/get-item.ts
+++ grums-client/app/actions/item/get-item.ts
@@ -1,13 +1,14 @@
 "use server"
 
+import { IItem } from "@/app/common/interfaces/item.interface";
 import {getById} from "@/app/common/util/server-requests";
 
-export default async function getItemById(itemId: string) {
+export default async function getItemById(itemId: string) : Promise<IItem> {
     try {
     const data = await getById(`clover/inventory/${itemId}`, itemId);
     return data;
     } catch (error) {
     console.error("Fetch Error:", error);
-    return [];
+    throw error;
     }
 }
